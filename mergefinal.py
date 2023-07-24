20d246384159a25099ad8f5f33d197fcd8fda5a4
ipoizzen <136999736+ipoizzen@users.noreply.github.com>
2023-07-23 21:49:07 -0400

    Update mergefinal.py

--- mergefinal.py
+++ mergefinal.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, classification_report
 from keras.models import load_model
-from keras.models import Model
-from keras.models import save_model
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+import keras  # Import keras explicitly for the callback
 
 # Load the trained models
 model1 = load_model('kddresults/dnn3layer/dnn3layer_model.hdf5')
@@ -17,52 +18,94 @@ testdata = pd.read_csv('https://raw.githubusercontent.com/rahulvigneswaran/Intru
 X_test = testdata.iloc[:, :-1].values  # Features
 y_test = testdata.iloc[:, -1].values   # Labels
 
-# Make predictions using the reference models
-y_pred_model1 = np.argmax(model1.predict(X_test), axis=-1)
-y_pred_model2 = np.argmax(model2.predict(X_test), axis=-1)
+# Convert labels to integer type
+y_test = y_test.astype(int)
 
-# Create a new IDS model using the predictions of the reference models
-# Assuming a majority voting approach
-y_pred_new = np.round((y_pred_model1 + y_pred_model2) / 2).astype(int)
+# Standardize the input features
+scaler = StandardScaler()
+X_test = scaler.fit_transform(X_test)
 
-# Encode labels if necessary
-label_encoder = LabelEncoder()
-label_encoder.fit(y_test)
+# Create a validation set to tune the ensemble weights
+X_train, X_val, y_train, y_val = train_test_split(X_test, y_test, test_size=0.3, random_state=42)
 
-# Handle unseen labels
-unique_labels_test = np.unique(y_test)
-unique_labels_pred = np.unique(y_pred_new)
-unseen_labels = np.setdiff1d(unique_labels_pred, unique_labels_test)
+# Convert labels to integer type
+y_val = y_val.astype(int)
 
-missing_label = -1  # Replace with a valid label value
-label_encoder.classes_ = np.append(label_encoder.classes_, missing_label)
+# Define a custom callback to record accuracy during training
+class AccuracyHistoryCallback(keras.callbacks.Callback):
+    def __init__(self, X_val):
+        self.X_val = X_val
+        self.accuracy_history = []
+        self.best_accuracy = 0.0
 
-# Map unseen labels to missing label
-y_pred_new = np.where(np.isin(y_pred_new, unseen_labels), missing_label, y_pred_new)
+    def on_epoch_end(self, epoch, logs={}):
+        y_pred_weighted = (best_weight_model1 * model1.predict(self.X_val) + best_weight_model2 * model2.predict(self.X_val)) / 2
+        accuracy = keras.metrics.sparse_categorical_accuracy(y_val, y_pred_weighted)
+        self.accuracy_history.append(np.mean(accuracy))
 
-# Encode the labels
-y_test_encoded = label_encoder.transform(y_test)
-y_pred_new_encoded = label_encoder.transform(y_pred_new)
+        # Keep track of the best accuracy
+        if np.mean(accuracy) > self.best_accuracy:
+            self.best_accuracy = np.mean(accuracy)
 
-# Define the threshold for binary classification
-threshold = 0.5
+# Create an instance of the callback with X_val
+accuracy_history_callback = AccuracyHistoryCallback(X_val)
 
-# Apply threshold to obtain binary predictions
-y_pred_thresholded = (y_pred_new_encoded >= threshold).astype(int)
+best_accuracy = 0.0
+best_weight_model1 = 0.0
+best_weight_model2 = 0.0
 
-# Evaluate the model using the provided reference
-accuracy = accuracy_score(y_test_encoded, y_pred_thresholded)
-recall = recall_score(y_test_encoded, y_pred_thresholded, average='micro')
-precision = precision_score(y_test_encoded, y_pred_thresholded, average='micro')
-f1 = f1_score(y_test_encoded, y_pred_thresholded, average='micro')
+# Try different weight combinations for the ensemble
+for weight_model1 in np.arange(0.05, 1.0, 0.05):
+    weight_model2 = 1.0 - weight_model1
+
+    # Weighted merging of predictions
+    y_pred_weighted = (weight_model1 * model1.predict(X_val) + weight_model2 * model2.predict(X_val)) / 2
+    accuracy = keras.metrics.sparse_categorical_accuracy(y_val, y_pred_weighted)
+
+    # Keep track of the best weight combination
+    if np.mean(accuracy) > best_accuracy:
+        best_accuracy = np.mean(accuracy)
+        best_weight_model1 = weight_model1
+        best_weight_model2 = weight_model2
+
+# Save the accuracy of the best weight combination
+accuracy_file = "best_accuracy.txt"
+with open(accuracy_file, 'w') as f:
+    f.write(str(best_accuracy))
+
+print("Best Weight for Model1:", best_weight_model1)
+print("Best Weight for Model2:", best_weight_model2)
+
+# Train the callback model and record accuracy during training with early stopping
+early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=1)
+model_history = model1.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=5, batch_size=41, callbacks=[accuracy_history_callback, early_stopping])
+
+# Save the accuracy history to a NumPy file
+accuracy_history = {
+    'train_accuracy': model_history.history['accuracy'],
+    'val_accuracy': model_history.history['val_accuracy']
+}
+
+np.save("kddresults/testing/accuracy_history.npy", accuracy_history)
+
+# Use the best weight combination to make predictions on the test set
+y_pred_weighted = (best_weight_model1 * model1.predict(X_test) + best_weight_model2 * model2.predict(X_test)) / 2
+accuracy = keras.metrics.sparse_categorical_accuracy(y_test, y_pred_weighted)
 
 print("----------------------------------------------")
-print("accuracy")
-print("%.3f" % accuracy)
-print("recall")
-print("%.3f" % recall)
-print("precision")
-print("%.3f" % precision)
-print("f1score")
-print("%.3f" % f1)
+print("Best Accuracy during Training:", accuracy_history_callback.best_accuracy)
+print("Final Accuracy on Test Set:", np.mean(accuracy))
 
+# Print the classification report
+y_pred_weighted_labels = np.argmax(y_pred_weighted, axis=1)
+classification_rep = classification_report(y_test, y_pred_weighted_labels, zero_division=1)
+print("Classification Report:\n", classification_rep)
+
+# Plot the accuracy history during training
+plt.plot(range(1, len(model_history.history['accuracy']) + 1), model_history.history['accuracy'], label='Training Accuracy')
+plt.plot(range(1, len(model_history.history['val_accuracy']) + 1), model_history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.grid(True)
+plt.show()
